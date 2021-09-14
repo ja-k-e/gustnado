5adc96d0d49bff32fbbd20c63ba79a42a09c762d
Jake Albaugh <jake.albaugh@gmail.com>
2021-09-14 21:32:27 +0000

    save

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ function createSection({
         ${li(`${pobabs}/${pobab}`, "keep")}
         ${pobm ? li(pobm, "POBM") : ""}
         ${li(abro, "ABRO", true)}
-        ${li(ie, "<strike>inning</strike>")}
+        ${li(ie, "inning KO")}
       </ul>
     </div>
   `;
