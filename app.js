e61d3b8fdc69d0b33bb287807821d8b8fef69041
Jake Albaugh <jake.albaugh@gmail.com>
2021-09-14 21:28:52 +0000

    save

--- app.js
+++ app.js
@@ -1,60 +1,127 @@
 const data = getData();
 const container = document.getElementById("container");
 
-const plural = (string, number) => `${string}${number > 1 ? "s" : ""}`;
+const plural = (string, number) => `${string}${number === 1 ? "" : "s"}`;
 
-data.forEach((player) => {
-  if (player.player) {
-    const hits = player.single + player.double + player.triple + player.homer;
-    player.avg = (hits / (player.ab - player.walk)).toFixed(3);
-    player.obp = (((hits + player.walk) / player.ab) * 100).toFixed(1);
-    player.bags =
-      player.single + player.double * 2 + player.triple * 3 + player.homer * 4;
-    player.slug = (player.bags / (player.ab - player.walk)).toFixed(3);
-    createSection(player);
+data.forEach((playerData) => {
+  const {
+    player, // name
+    h1, // singles
+    h2, // doubles
+    h3, // triples
+    hr, // homers
+    bb, // walks
+    ab, // plate appearances
+    abo, // plate appearance outs
+    abro, // plate appearances that resulted in outs
+    o, // total outs
+    rbi, // runs batted in
+    s, // scores
+    ie, // innings ended
+    sio, // start it off
+    sior, // start it off right
+    pob, // players on base
+    pobm, // players on base moved
+    g,
+  } = playerData;
+  if (player) {
+    const hits = h1 + h2 + h3 + hr;
+    // batting average (hits / at bats)
+    playerData.avg = (hits / (ab - bb)).toFixed(3);
+    // on base percentage (on base / plate appearances)
+    playerData.obp = (((hits + bb) / ab) * 100).toFixed(1);
+    // total bases
+    playerData.bags = h1 + h2 * 2 + h3 * 3 + hr * 4;
+    // slugging %
+    playerData.slug = (playerData.bags / (ab - bb)).toFixed(3);
+    // players on base moved rate
+    playerData.pobmr = `${Math.round((pobm / (pob || 1)) * 100)}%`;
+    // start it off right rate
+    playerData.siorr = `${Math.round((sior / (sio || 1)) * 100)}%`;
+    // total outs per plate appearance
+    playerData.outr = `${Math.round((o / ab) * 100)}%`;
+    // scores per gam
+    playerData.spg = (s / g).toFixed(1);
+    // at bat outs per game
+    playerData.opg = (abo / g).toFixed(1);
+    // bags per game
+    playerData.bpg = (playerData.bags / g).toFixed(1);
+    // hits per game
+    playerData.hpg = (hits / g).toFixed(1);
+    // rbis per game
+    playerData.rbipg = (rbi / g).toFixed(1);
+    // innings ended per game
+    playerData.iepg = (ie / g).toFixed(1);
+    createSection(playerData);
   }
 });
 
 function createSection({
-  player,
-  games,
-  single,
-  double,
-  triple,
-  homer,
-  walk,
-  score,
   ab,
-  out,
-  bags,
-  slug,
-  rbi,
+  abo,
+  abro,
   avg,
+  bags,
+  bpg,
+  g,
+  h1,
+  h2,
+  h3,
+  hpg,
+  hr,
+  ie,
+  iepg,
   obp,
+  opg,
+  player,
+  pobab,
+  pobabs,
+  pobm,
+  rbi,
+  rbipg,
+  s,
+  sioab,
+  siorab,
+  slug,
+  spg,
 }) {
   const section = document.createElement("section");
-  const li = (start, end) => `<li><strong>${start}&nbsp;${end}</strong></li>`;
-  const conditionalLi = (stat, word, pluralize = true) =>
-    stat ? li(stat, `${pluralize ? plural(word, stat) : word}`) : "";
+  const li = (stat, word, pluralize = false, extra = "") =>
+    `<li><strong>${stat}&nbsp;<label>${
+      pluralize ? plural(word, stat) : word
+    }&nbsp;${extra}</label></strong></li>`;
   section.innerHTML = `
     <h3>${player}</h3>
     <div class="stats">
       <ul>
         <li class="double-line"></li>
-        <li><strong>${avg}&nbsp;AVG</strong></li>
-        <li><strong>${slug}&nbsp;SLG</strong></li>
-        <li><strong>${obp}%&nbsp;OBP</strong></li>
-        ${homer || triple || double || single ? '<li class="line"></li>' : ""}
-        ${conditionalLi(homer, "dinger")}
-        ${conditionalLi(triple, "triple")}
-        ${conditionalLi(double, "double")}
-        ${conditionalLi(single, "single")}
-        ${rbi || score || bags ? '<li class="line"></li>' : ""}
-        ${conditionalLi(rbi, "RBI", false)}
-        ${conditionalLi(score, "run")}
-        ${conditionalLi(bags, "base")}
-        </ul>
-      <ul></ul>
+        ${li(avg, "AVG")}
+        ${li(slug, "SLG")}
+        ${li(`${obp}%`, "OBP")}
+        ${bags ? '<li class="line"></li>' : ""}
+        ${hr ? li(hr, "dinger", true) : ""}
+        ${h3 ? li(h3, "triple", true) : ""}
+        ${h2 ? li(h2, "double", true) : ""}
+        ${h1 ? li(h1, "single", true) : ""}
+        ${rbi || s || bags ? '<li class="line"></li>' : ""}
+        ${rbi ? li(rbi, "RBI", true) : ""}
+        ${s ? li(s, "run", true, "scored") : ""}
+        ${bags ? li(bags, "total bag", true) : ""}
+        <li class="line"></li>
+        <li class="label"><strong><label>Per Game (${g})</label></strong></li>
+        ${li(hpg, "hit", true)}
+        ${li(rbipg, "RBI", true)}
+        ${li(bpg, "bag", true)}
+        ${li(spg, "score", true)}
+        ${li(opg, "out", true)}
+        <li class="line"></li>
+        <li class="label"><strong><label>Momentum (${ab})</label></strong></li>
+        ${li(`${siorab}/${sioab}`, "lead")}
+        ${li(`${pobabs}/${pobab}`, "keep")}
+        ${pobm ? li(pobm, "POBM") : ""}
+        ${li(abro, "ABRO", true)}
+        ${li(ie, "<strike>inning</strike>")}
+      </ul>
     </div>
   `;
   container.appendChild(section);
@@ -64,198 +131,223 @@ function getData() {
   return [
     {
       player: "Bryan",
-      games: 1,
-      single: 0,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
+      h1: 0,
+      h2: 0,
+      h3: 0,
+      hr: 0,
+      bb: 0,
       ab: 3,
-      out: 3,
+      abo: 3,
+      abro: 3,
+      o: 3,
       rbi: 0,
-      score: 0,
+      s: 0,
+      ie: 0,
+      sioab: 1,
+      siorab: 0,
+      pobab: 2,
+      pobabs: 0,
+      pob: 5,
+      pobm: 0,
+      g: 1,
     },
     {
       player: "Cory",
-      games: 1,
-      single: 1,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 1,
+      h1: 1,
+      h2: 0,
+      h3: 0,
+      hr: 0,
+      bb: 1,
       ab: 3,
-      out: 1,
+      abo: 1,
+      abro: 1,
+      o: 1,
       rbi: 0,
-      score: 1,
+      s: 1,
+      ie: 1,
+      sioab: 0,
+      siorab: 0,
+      pobab: 3,
+      pobabs: 2,
+      pob: 5,
+      pobm: 3,
+      g: 1,
     },
     {
       player: "Erik",
-      games: 1,
-      single: 0,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
+      h1: 0,
+      h2: 0,
+      h3: 0,
+      hr: 0,
+      bb: 0,
       ab: 4,
-      out: 4,
+      abo: 4,
+      abro: 4,
+      o: 4,
       rbi: 0,
-      score: 0,
+      s: 0,
+      ie: 2,
+      sioab: 0,
+      siorab: 0,
+      pobab: 4,
+      pobabs: 0,
+      pob: 6,
+      pobm: 0,
+      g: 1,
     },
     {
       player: "Geoff",
-      games: 1,
-      single: 2,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
+      h1: 2,
+      h2: 0,
+      h3: 0,
+      hr: 0,
+      bb: 0,
       ab: 4,
-      out: 2,
+      abo: 2,
+      abro: 2,
+      o: 2,
       rbi: 1,
-      score: 1,
+      s: 1,
+      ie: 1,
+      sioab: 2,
+      siorab: 1,
+      pobab: 2,
+      pobabs: 1,
+      pob: 2,
+      pobm: 1,
+      g: 1,
     },
     {
       player: "Jake",
-      games: 1,
-      single: 2,
-      double: 1,
-      triple: 1,
-      homer: 0,
-      walk: 0,
+      h1: 2,
+      h2: 1,
+      h3: 1,
+      hr: 0,
+      bb: 0,
       ab: 4,
-      out: 0,
+      abo: 0,
+      abro: 0,
+      o: 0,
       rbi: 7,
-      score: 1,
+      s: 1,
+      ie: 0,
+      sioab: 0,
+      siorab: 0,
+      pobab: 4,
+      pobabs: 4,
+      pob: 9,
+      pobm: 9,
+      g: 1,
     },
     {
       player: "John",
-      games: 1,
-      single: 2,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
+      h1: 2,
+      h2: 0,
+      h3: 0,
+      hr: 0,
+      bb: 0,
       ab: 4,
-      out: 2,
+      abo: 2,
+      abro: 4,
+      o: 2,
       rbi: 0,
-      score: 0,
+      s: 0,
+      ie: 1,
+      sioab: 1,
+      siorab: 0,
+      pobab: 3,
+      pobabs: 1,
+      pob: 5,
+      pobm: 1,
+      g: 1,
     },
     {
       player: "Jonny",
-      games: 1,
-      single: 1,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 1,
+      h1: 1,
+      h2: 0,
+      h3: 0,
+      hr: 0,
+      bb: 1,
       ab: 4,
-      out: 2,
-      rbi: 0,
-      score: 2,
+      abo: 2,
+      abro: 2,
+      o: 2,
+      rbi: 1,
+      s: 2,
+      ie: 1,
+      sioab: 3,
+      siorab: 1,
+      pobab: 1,
+      pobabs: 0,
+      pob: 6,
+      pobm: 3,
+      g: 1,
     },
     {
       player: "Matt",
-      games: 1,
-      single: 2,
-      double: 1,
-      triple: 0,
-      homer: 0,
-      walk: 0,
+      h1: 2,
+      h2: 1,
+      h3: 0,
+      hr: 0,
+      bb: 0,
       ab: 4,
-      out: 3,
+      abo: 1,
+      abro: 1,
+      o: 3,
       rbi: 0,
-      score: 1,
+      s: 1,
+      ie: 0,
+      sioab: 2,
+      siorab: 1,
+      pobab: 2,
+      pobabs: 2,
+      pob: 2,
+      pobm: 2,
+      g: 1,
     },
     {
       player: "Paul",
-      games: 1,
-      single: 3,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
+      h1: 3,
+      h2: 0,
+      h3: 0,
+      hr: 0,
+      bb: 0,
       ab: 4,
-      out: 1,
+      abo: 1,
+      abro: 1,
+      o: 1,
       rbi: 0,
-      score: 1,
+      s: 1,
+      ie: 0,
+      sioab: 0,
+      siorab: 0,
+      pobab: 4,
+      pobabs: 4,
+      pob: 5,
+      pobm: 5,
+      g: 1,
     },
     {
       player: "Travis",
-      games: 1,
-      single: 2,
-      double: 2,
-      triple: 0,
-      homer: 0,
-      walk: 0,
+      h1: 2,
+      h2: 2,
+      h3: 0,
+      hr: 0,
+      bb: 0,
       ab: 4,
-      out: 0,
+      abo: 0,
+      abro: 0,
+      o: 0,
       rbi: 2,
-      score: 4,
-    },
-    {
-      player: "",
-      games: 0,
-      single: 0,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
-      ab: 0,
-      out: 0,
-      rbi: 0,
-      score: 0,
-    },
-    {
-      player: "",
-      games: 0,
-      single: 0,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
-      ab: 0,
-      out: 0,
-      rbi: 0,
-      score: 0,
-    },
-    {
-      player: "",
-      games: 0,
-      single: 0,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
-      ab: 0,
-      out: 0,
-      rbi: 0,
-      score: 0,
-    },
-    {
-      player: "",
-      games: 0,
-      single: 0,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
-      ab: 0,
-      out: 0,
-      rbi: 0,
-      score: 0,
-    },
-    {
-      player: "",
-      games: 0,
-      single: 0,
-      double: 0,
-      triple: 0,
-      homer: 0,
-      walk: 0,
-      ab: 0,
-      out: 0,
-      rbi: 0,
-      score: 0,
+      s: 4,
+      ie: 0,
+      sioab: 2,
+      siorab: 2,
+      pobab: 2,
+      pobabs: 2,
+      pob: 4,
+      pobm: 4,
+      g: 1,
     },
   ];
 }
